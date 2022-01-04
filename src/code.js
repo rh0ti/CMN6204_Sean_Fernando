0822320148d66cb9a7ac2a8771ec04a895a5dfb6
Rhottana Truy <rhottana.truy@gmail.com>
2022-01-04 16:04:52 +0100

    added css

---
+++ src/code.js
@@ -0,0 +1,36 @@
+//-------------------------------------------------------------- ScrollUp Button ---------------------------------------------------------------//
+$(document).ready(function (){
+
+    //While scrolling up the topBtn will slowly disappear
+    
+      $(window).scroll(function(){
+        if($(this).scrollTop() > 40){
+          $('#topBtn').fadeIn();
+        }else{
+          $('#topBtn').fadeOut();
+        }
+      })
+      
+      //When click on the topBtn-Button the side will Scroll to the top
+    
+      $("#topBtn").click(function(){
+        // $(console.log) - Debug-Logs
+        $('html ,body').animate({
+        scrollTop : 0},800);
+      });
+    
+    });
+
+    
+    //-------------------------------------------------------------- Sidebar ---------------------------------------------------------------//
+
+$(document).ready(function(){
+
+    //By clicking the Burgermenu (SidebarBtn), the sidebar will show. When click on Bergermenu again it will close.(Toggle)
+        $('.sidebarBtn').click(function(){
+                  //$(console.log) - Debug-Logs
+            $('.sidebar').toggleClass('active');
+            $('.sidebarBtn').toggleClass('toggle');
+        })
+    
+    })
