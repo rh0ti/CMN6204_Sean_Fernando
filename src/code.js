bea423f68772ef95b601b40f87e180943caf23cb
Rhottana Truy <rhottana.truy@gmail.com>
2022-01-19 16:32:19 +0100

    added intro

--- src/code.js
+++ src/code.js
@@ -37,8 +37,43 @@ $(document).ready(function(){
 
 //-------------------------------------------------------------- Circle ---------------------------------------------------------------//
 
-const text = document.querySelector('.text p');
-text.innerHTML = text.innerText.split("").map(
-  (char, i) =>
-  `<span style="transform:rotate(${i * 7}deg)">${char}</span>`
-).join("")
+// const text = document.querySelector('.text p');
+// text.innerHTML = text.innerText.split("").map(
+//   (char, i) =>
+//   `<span style="transform:rotate(${i * 7}deg)">${char}</span>`
+// ).join("")
+
+//-------------------------------------------------------------- Intro ---------------------------------------------------------------//
+
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMcontentLoaded', ()=>{
+
+  setTimeout(()=>{
+
+    logoSpan.forEach((span, idx)=>{
+      setTimeout(()=>{
+        span.classList.add('active');
+      }, (idx + 1) * 400)
+    });
+
+    setTimeout(()=>{
+
+      logoSpan.forEach((span, idx)=>{
+
+        setTimeout(()=>{
+          span.classList.remove('active');
+          span.classList.fade('fade');
+        }, (idx + 1) * 50)
+      })
+      
+      },2000);
+
+      setTimeout(()=>{
+        intro.style.top = '-100vh';
+      })
+
+  })
+})
