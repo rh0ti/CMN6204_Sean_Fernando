9bca20419a4faa77780389c9ae7c0ccf3f073434
Rhottana Truy <rhottana.truy@gmail.com>
2022-01-25 15:09:08 +0100

    liquid images changes

--- src/code.js
+++ src/code.js
@@ -84,8 +84,8 @@ $(document).ready(function(){
 new hoverEffect({
   parent: document.querySelector('.distortion'),
   intensity: 0.2,
-  image1: './images/bw1.jpg',
+  image1: '/images/bw1.jpg',
   image2: '/images/bw2.jpg',
-  displacementImage: '/images/diss.png'
+  displacementImage: './images/diss.png'
 });
 
