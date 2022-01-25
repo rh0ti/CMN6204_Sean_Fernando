d162c0f9d34819ec00c485b1541a65ce2ab90f9a
Rhottana Truy <rhottana.truy@gmail.com>
2022-01-25 14:42:52 +0100

    add liquid transition

--- src/code.js
+++ src/code.js
@@ -79,3 +79,13 @@ $(document).ready(function(){
 // ).join("")
 
 
+//-------------------------------------------------------------- Hover Effect ---------------------------------------------------------------//
+
+new hoverEffect({
+  parent: document.querySelector('.distortion'),
+  intensity: 0.2,
+  image1: './images/bw1.jpg',
+  image2: '/images/bw2.jpg',
+  displacementImage: '/images/diss.png'
+});
+
